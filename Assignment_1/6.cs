3fd515253961fd2c6b895f1033a4cbb01fb590a2
Abhibhav Om Tyagi <101652744+Overpowering-Victorious@users.noreply.github.com>
2025-07-30 23:03:32 +0530

    Create 6.cs

---
+++ Assignment_1/6.cs
@@ -0,0 +1,7 @@
+Console.WriteLine("Enter string: ");
+string s1=Console.ReadLine();
+string s2 = s1;
+
+Console.WriteLine("First string: " + s1);
+Console.WriteLine("Second string: " + s2);
+Console.WriteLine("Number of characters copied: " + s1.Length);
